db87aeb1040b1db419630ef7be83aedce1b4a7e1
LauraR <ramirezochoa08@gmail.com>
2024-10-28 20:33:27 -0500

    cambios de clase

--- registrarGastos/registrarGastos.js
+++ registrarGastos/registrarGastos.js
@@ -1,21 +1,26 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+    if(false){
+        window.location.href = '../index.html'
+    }
+})
+
 document.getElementById('gastoForm').addEventListener('submit', function (event) {
     event.preventDefault();
 
     const descripcion = document.getElementById('descripcion').value;
     const monto = document.getElementById('monto').value;
     const fecha = document.getElementById('fecha').value;
+    const categoria = document.getElementById('categoria').value;
 
-    if (!descripcion || !monto || !fecha) {
+    if (!descripcion || !monto || !fecha || !categoria) {
         alert('Por favor, rellena todos los campos.');
         return;
     }
 
+    const usuario = localStorage.getItem('email');
+
     // Almacenar el gasto en localStorage
-    const gasto = {
-        descripcion,
-        monto,
-        fecha
-    };
+    const gasto = { descripcion, monto, fecha, categoria, usuario };
 
     let gastos = JSON.parse(localStorage.getItem('gastos')) || [];
     gastos.push(gasto);
